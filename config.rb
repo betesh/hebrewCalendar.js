633faf4189ba07d94d3cecddc0537109362e9d6e
Isaac Betesh <iybetesh@gmail.com>
2019-09-19 23:57:32 -0400

    Configure for easy uploading to AWS S3

--- config.rb
+++ config.rb
@@ -46,6 +46,23 @@ configure :build do
   activate :gzip
 end
 
+aws_yaml = YAML::load(File.open('aws.yml'))
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = aws_yaml['bucket']
+  s3_sync.aws_access_key_id          = aws_yaml['credentials']['access_key_id']
+  s3_sync.aws_secret_access_key      = aws_yaml['credentials']['secret_access_key']
+  s3_sync.delete                     = true
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+end
+
 activate :sprockets
 
 require 'webrick'
